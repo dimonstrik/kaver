72236656f33267637fc655e94156b1a8ae489eb5
dimonstrik88 <dimonstrik88@gmail.com>
2019-05-14 04:41:26 +0300

    bugfix

--- site/Startup.cs
+++ site/Startup.cs
@@ -48,7 +48,7 @@ namespace site
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
 
