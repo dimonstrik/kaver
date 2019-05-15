61aab194f0f7dacda126d413e05cc396076df859
dimonstrik88 <dimonstrik88@gmail.com>
2019-05-15 04:19:50 +0300

    * AboutController.cs: favicon added server configs and scripts improved
    * deploy.sh:
    * Program.cs:
    * Startup.cs:
    * site.csproj:
    * kaver.conf:
    * preparing.sh:
    * favicon.ico:
    * Index.cshtml.cs:
    * _Layout.cshtml:
    * kestrel-kaver.service:
    * Error.cshtml.cs:
    * HomeController.cs:
    * PortfolioController.cs:

--- site/deploy.sh
+++ site/deploy.sh
@@ -1,1 +1,3 @@
-﻿/usr/share/dotnet/dotnet publish --configuration release -o /var/www/kaver/
+﻿rm -rf /var/www/kaver/
+mkdir /var/www/kaver
+cd /kaver/kaver/site/ && /usr/share/dotnet/dotnet publish --configuration release -o /var/www/kaver/
