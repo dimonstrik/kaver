efae1ee0dc1416a49bb063e9ba53c9f13ff51dcb
dimonstrik88 <dimonstrik88@gmail.com>
2019-05-15 04:39:36 +0300

    sc changes

--- site/deploy.sh
+++ site/deploy.sh
@@ -1,3 +1,4 @@
-﻿rm -rf /var/www/kaver/
-mkdir /var/www/kaver
-cd /kaver/kaver/site/ && /usr/share/dotnet/dotnet publish --configuration release -o /var/www/kaver/
+﻿#!/bin/sh
+rm -rf /var/www/kaver;
+mkdir /var/www/kaver;
+cd /kaver/kaver/site/ && /usr/share/dotnet/dotnet publish --configuration release -o /var/www/kaver/;
